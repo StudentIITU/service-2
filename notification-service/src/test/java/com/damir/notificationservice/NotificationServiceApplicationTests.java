459fecb38a6b3063dd8527fe9835802cc91dee74
StudentIITU <96124721+StudentIITU@users.noreply.github.com>
2025-01-28 14:13:09 +0500

    Adding remaining subjects

---
+++ notification-service/src/test/java/com/damir/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.damir.notificationservice;
+
+import org.junit.jupiter.api.Test;
+
+class NotificationServiceApplicationTests extends AbstractIT {
+
+    @Test
+    void contextLoads() {}
+}
